37cde3e7b9fc04a8a45d20be6966fb31987af169
Sebastian Sauer <ssauer@posteo.de>
2022-03-08 12:25:02 +0100

    ex 4

--- 22-SoSe/FAU/2022-03-08.R
+++ 22-SoSe/FAU/2022-03-08.R
@@ -176,7 +176,52 @@ flights %>%
 # Exercise:
 
 # 1) What's the median delay in each NYC airport in Jan 2013?
-# 2) What's the average delay in each month in NYC?
+# 2) What's the average delay in each month in NYC? (across all airports, not grouped by airport/origin)
+# 3) BONUS: 2) and then find the month with the max value of delay!
+# 4) BONUS: 2) but also group by the origin!
 
 
-  
+
+# ex 1)
+flights %>%
+  filter(month == 1) %>% 
+  select(origin, dep_delay) %>% 
+  drop_na() %>% 
+  group_by(origin) %>% 
+  summarise(delay_median_per_origin = median(dep_delay))
+
+
+
+# ex #2)
+
+flights %>%
+  filter(origin == "JFK" | origin == "EWR" | origin == "LGA") %>% 
+  select(month, dep_delay) %>% 
+  drop_na() %>% 
+  group_by(month) %>% 
+  summarise(delay_avg_per_month = mean(dep_delay))
+
+# 4) BONUS: 2) but also group by the origin!
+
+# 4
+
+flights_ex4 <- 
+  flights %>%
+  select(month, dep_delay, origin) %>%
+  drop_na() %>%
+  group_by(month, origin) %>%
+  summarise(delay_avg_per_month = mean(dep_delay))
+
+
+# reshape it using pivot_wider()
+
+# ad 3) BONUS: 2) and then find the month with the max value of delay!
+
+flights %>%
+  select(month, dep_delay) %>%
+  drop_na() %>%
+  group_by(month) %>%
+  summarise(delay_avg_per_month = mean(dep_delay)) %>% 
+  arrange(-delay_avg_per_month) %>% 
+  #filter(row_number() == 1)
+  slice_head(n = 3)  # either via filter or via slice_head
