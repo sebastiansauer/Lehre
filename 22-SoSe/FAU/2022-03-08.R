8b5200a96eedd41db1736b2ee480446a8118cdaa
Sebastian Sauer <ssauer@posteo.de>
2022-03-08 10:25:11 +0100

    more vis

--- 22-SoSe/FAU/2022-03-08.R
+++ 22-SoSe/FAU/2022-03-08.R
@@ -35,6 +35,24 @@ ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) +
   geom_point(alpha = 0.2)
 
 
+# Geom "bin2d":
 ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
   geom_bin2d()
 
+
+
+# Geom "hex":
+ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+  geom_hex()
+
+
+
+# Geom "density 2d filled":
+ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+  geom_density_2d_filled()
+
+
+
+# Geom "density 2d filled":
+ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+  geom_density_2d()
