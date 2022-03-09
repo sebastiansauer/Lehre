4eada5a87f341159216475ac14cfe5f95e7a5e9b
Sebastian Sauer <ssauer@posteo.de>
2022-03-09 10:31:47 +0100

    exs regresssion simple

--- 22-SoSe/FAU/2022-03-09.R
+++ 22-SoSe/FAU/2022-03-09.R
@@ -32,3 +32,52 @@ data(evals)
 # 3) Visualize the assocation of beauty and evaluation!
 # 4) Compute the correlation!
 
+
+
+# ad 3)
+
+
+
+ggplot(evals_ch5, aes(x = bty_avg, y = score)) +
+  geom_jitter() +
+  labs(x = "Beauty Score", 
+       y = "Teaching Score",
+       title = "Scatterplot of relationship of teaching and beauty scores") +
+  geom_smooth(method = "lm", se = FALSE) 
+
+
+# ad 4)
+
+
+evals_ch5 %>% 
+  summarise(cor(bty_avg, score))
+
+.187^2
+
+evals_ch5 %>% 
+  get_correlation(score ~ bty_avg)
+
+
+# R formula: "y ~ x"  - > y = f(x)
+
+
+
+
+lm1 <- lm(score ~ bty_avg, data = evals_ch5)
+
+
+
+
+# Get regression table (for coefficients):
+get_regression_table(lm1)
+
+
+# Get summary (for R squared and more):
+summary(lm1)
+
+
+
+# Ex
+# lm: score ~ age, intercept, b, R^2 
+
+
