47368d452cd7cfa7a1a004ca644fd1f77ab47339
Sebastian Sauer <ssauer@posteo.de>
2022-03-09 09:58:26 +0100

    more exs

--- 22-SoSe/FAU/2022-03-09.R
+++ 22-SoSe/FAU/2022-03-09.R
@@ -26,5 +26,9 @@ data(evals)
 
 
 # Exercises:
+
 # 1) Provide (invent) an alternative *causal* explanation of the correlation of beauty and teaching evaluation (where beauty is not the cause of teaching evaluation)!
 # 2) What's the difference between a correlation coefficient (r) and a regression coefficient (b)?
+# 3) Visualize the assocation of beauty and evaluation!
+# 4) Compute the correlation!
+
