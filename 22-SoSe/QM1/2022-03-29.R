cff43bf5600634d65395f0b930adfbb83d41f93f
Sebastian Sauer <ssauer@posteo.de>
2022-03-29 14:42:28 +0200

    update

--- 22-SoSe/QM1/2022-03-29.R
+++ 22-SoSe/QM1/2022-03-29.R
@@ -1,6 +1,12 @@
 # Syntax 2022-03-29
 
-
+library(tidyverse)
 data(mtcars)
 
 # Filter mit die Karren mit 8 Zylindern
+
+filter(mtcars, cyl < 6)
+# synonym:
+filter(mtcars, cyl == 8)  # ZWEI !!! Istgleichzeichen
+
+# Aufgabe: Filtern Sie alle Karren mit Automatik-Getriebe
