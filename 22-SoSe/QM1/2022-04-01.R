bc0f62fb530d26511ea601df231646c9b7038059
Sebastian Sauer <ssauer@posteo.de>
2022-04-01 10:38:03 +0200

    update

--- 22-SoSe/QM1/2022-04-01.R
+++ 22-SoSe/QM1/2022-04-01.R
@@ -7,8 +7,12 @@ data(mtcars)
 # Exercise:
 # Take mtcars, select am, hp, mpg, filter hp > 100, group by am, summarise mpg mean
 
+# Shortcut for the pipe: Cntl-Shift-M %>% 
+
+mtcars_cool_pipeline <-
 mtcars %>% 
   select(am, hp, mpg) %>% 
   filter(hp > 100) %>% 
   group_by(am) %>% 
-  summarise(mpg_mean_per_group = mean(mpg))
+  summarise(mean(mpg))
+
