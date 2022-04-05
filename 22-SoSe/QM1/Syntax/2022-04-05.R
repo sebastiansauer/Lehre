4981d2dc3a9b7c0b1539ef15ab3f66fe7f15f019
Sebastian Sauer <ssauer@posteo.de>
2022-04-05 15:25:16 +0200

    syntax

---
+++ 22-SoSe/QM1/Syntax/2022-04-05.R
@@ -0,0 +1,37 @@
+# 2022-04-05, QM1
+
+
+# MAD vs. SD
+
+# Definiere die Messwerte:
+x <- c(1, 10, 11)
+
+# MAD:
+# 1. Abstände d_i von x_i zum MW
+# 2. MW d_i
+
+# Berechne MW:
+x_m <- mean(x)
+x_m
+
+# Berechne Abstand der Messwerte zum MW von X:
+d <- x - x_m
+
+# Berechne Absolutabstände:
+d_abs <- abs(d)
+
+# MAD als MW der Abstolutabstände:
+mad_x <- mean(d_abs)
+
+
+
+# Varianz/SD
+
+d_sq <- d^2
+d_sq
+
+var_x <- mean(d_sq)
+var_x    
+
+sd_x <- sqrt(var_x)
+sd_x
