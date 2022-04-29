d6e858241c23c3608ff03d246f53b82c271950af
Sebastian Sauer <ssauer@posteo.de>
2022-04-29 10:29:38 +0200

    syntax

---
+++ 22-SoSe/QM1/Syntax/2022-04-29.R
@@ -0,0 +1,22 @@
+# Praxisprobleme
+
+
+data(mtcars)
+library(tidyverse)
+
+mtcars$mpg[1] <- NA  # NA erzeugen
+
+# Mean value of mpg:
+
+mtcars %>% 
+  summarise(mpg_mean = mean(mpg))
+
+
+# Zeilen mit fehlenden Werten löschen:
+
+mtcars %>% 
+  drop_na() %>%  # kicks out all rows with NAs
+  summarise(mpg_mean = mean(mpg))
+
+
+# Bonus: Compute median and SD of mpg as well!
