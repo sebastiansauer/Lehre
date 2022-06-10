7f456d9b48e82dbd4e1c2c19ab7557d0132c8541
Sebastian Sauer <ssauer@posteo.de>
2022-06-10 11:44:49 +0200

    syntax

--- 22-SoSe/QM1/Syntax/2022-06-10.R
+++ 22-SoSe/QM1/Syntax/2022-06-10.R
@@ -1,8 +1,13 @@
 # 2022-06-10
 
+
+# Daten importieren:
 data("mtcars")
+
+# Relevante Pakete laden:
 library(tidyverse)
 
+# Berechnung eines einfachen Modells:
 lm(mpg ~ hp, data = mtcars)
 
 mtcars %>% 
@@ -28,3 +33,34 @@ y
 
 # oder so:
 predict(lm1, newdata = tibble(hp = 200, am = 1))
+
+
+
+
+mtcars %>% 
+  mutate(am = factor(am)) %>% 
+  ggplot(aes(x = hp, y = mpg, color = am)) +
+  geom_point( ) +
+  geom_smooth(method = "lm")
+
+
+# Im Default, kein Interaktionseffekt zugelassen:
+lm1 <- lm(mpg ~ hp + am, data = mtcars)
+
+# Mit Interaktionseffekt:
+
+
+lm2 <- lm(mpg ~ hp + am + hp:am, data = mtcars)
+lm2
+
+
+predictions_tab <- 
+  mtcars %>% 
+  mutate(id = 1:32) %>% 
+  mutate(pred = predict(lm2, newdata = mtcars)) %>% 
+  select(id, pred)
+
+write_csv(predictions_tab, file = "submission_sauer_sebastian_JB07777.csv")
+
+
+
