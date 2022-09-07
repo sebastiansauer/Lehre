a83f4a26411de834fe338aea13910d27197d0dd1
Sebastian Sauer <ssauer@posteo.de>
2022-09-07 17:59:31 +0200

    update

--- 22-WiSe/FAU/r-packages-needed.R
+++ 22-WiSe/FAU/r-packages-needed.R
@@ -1,14 +1,19 @@
 # Welcome!
 
 
-# Please make sure the following R packages are installed on your PC (on the cloud version, all needed software is already installed):
+# Surf to the course site to get started: https://stats-nutshell.netlify.app
+
+
+# Please make sure the following R packages are installed on your PC 
+# (on the cloud version, all needed software is already installed):
 
 install.packages("tidyverse")
 install.packages("easystats")
 
 
 
-# The next R package is optional, unfortunately, some external software is needed, see here for more information: https://stats-nutshell.netlify.app/#upfront-student-preparation
+# The next R package is optional, unfortunately, some external software is needed to install the package
+# see here for more information: https://stats-nutshell.netlify.app/#upfront-student-preparation
 
 install.packages("rstanarm")
   
