f03655e2a1cc3947d4cb0ac3d57bd48f14110e3d
Sebastian Sauer <ssauer@posteo.de>
2022-09-07 17:57:52 +0200

    fau init

---
+++ 22-WiSe/FAU/r-packages-needed.R
@@ -0,0 +1,14 @@
+# Welcome!
+
+
+# Please make sure the following R packages are installed on your PC (on the cloud version, all needed software is already installed):
+
+install.packages("tidyverse")
+install.packages("easystats")
+
+
+
+# The next R package is optional, unfortunately, some external software is needed, see here for more information: https://stats-nutshell.netlify.app/#upfront-student-preparation
+
+install.packages("rstanarm")
+  
