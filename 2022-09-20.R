6845e96b434efe4e83dbdbb80534e8f1d7be290e
Sebastian Sauer <ssauer@posteo.de>
2022-09-20 11:37:04 +0200

    update

--- 2022-09-20.R
+++ 2022-09-20.R
@@ -8,7 +8,7 @@
 
 library(tidyverse)
 library(easystats)
-library(rstanarm)
+library(rstanarm)  # optional
 
 data("mtcars")
 
@@ -35,6 +35,41 @@ data(mtcars)
 # Compute this model: mpg ~ vs
 # Report the group means with their 95% CI
 
+
+
+
+mtcars4 <-
+  mtcars %>% 
+  mutate(vs_f = factor(vs)) 
+
+lm4a <- lm(mpg ~ vs_f, data = mtcars4)
+parameters(lm4a)
+
+lm4a_means <- estimate_means(lm4a, at = "vs")
+lm4a_means 
+
+plot(lm4a_means)
+
+
+# Ex: Use the data set "palmer penguins"
+# compute Model: weight as a function of species
+# Report group means
+
+d <- read.csv("https://vincentarelbundock.github.io/Rdatasets/csv/palmerpenguins/penguins.csv")
+
+
+
+
+
+
+
+
+
+
+
+lm5 <- lm(body_mass_g ~ species, data = d)
+lm5_means <- estimate_means(lm5)
+plot(lm5_means)
 
 
 
