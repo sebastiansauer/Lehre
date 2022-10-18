da4213fbbb8ac64e64858a5df458f096788380e2
Sebastian Sauer <ssauer@posteo.de>
2022-10-18 16:03:29 +0200

    bsp binomial

--- 22-WiSe/QM"/2022-10-18.R
+++ 22-WiSe/QM"/2022-10-18.R
@@ -12,9 +12,25 @@ x_simu_df <-
 x_simu_df %>% 
   count(x <= 5)
 
+# Binomialverteilung
 
+# Wie groß ist die Wahrscheinlichkeit,
+# bei 4 Münzwürfen 2 Treffer zu erzielen?
+# Für *einen* Ast beträgt die Wahrscheinlichkeit:
+
+p1 <- (2/5)^2 * (3/5)^2
+p1
 
 # Wie viele Möglichkeiten gibt es,
 # bei 4 Zügen 2 Treffer zu erzielen
 # (bei einer Binomialverteilung)?
 choose(4, 2)
+
+# Also beträgt die Summe der W. aller 6 Äste:
+
+p <- 6 * p1
+p
+
+# Das gleiche Ergebnis bekommt man so:
+
+dbinom(x = 2, size = 4, prob = 2/5)
