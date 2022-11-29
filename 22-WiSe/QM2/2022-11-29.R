39319b6f0d0e9ceff1bdd13f3510331ee656b579
Sebastian Sauer <ssauer@posteo.de>
2022-11-29 15:32:22 +0100

    Syntax 2022-11-29

---
+++ 22-WiSe/QM2/2022-11-29.R
@@ -0,0 +1,22 @@
+library(rstanarm)
+library(easystats)
+library(tidyverse)
+
+
+Kung_path <-  
+  "https://raw.githubusercontent.com/sebastiansauer/Lehre/main/data/Howell1a.csv"  
+
+d <- data_read(Kung_path)  # aus dem Paket `easystats`
+
+head(d)
+
+d2 <- d %>% 
+  filter(age >= 18)
+
+m41 <- stan_glm(height ~ 1, data = d2, refresh = 0)
+m41_post <- as_tibble(m41)  # Modellergebnis in Tabelle umwandeln
+names(m41_post) <- c("mu", "sigma")  # schönere Namen für die Spalten
+
+
+m41_post %>% 
+  count(mu > 154 & mu < 155)
