a557cc48160b69c18f2a136d1638440f4fcd7611
Sebastian Sauer <ssauer@posteo.de>
2023-02-28 10:31:04 +0100

    updatre

--- 23-SoSe/FAU/2023-02-28.R
+++ 23-SoSe/FAU/2023-02-28.R
@@ -48,5 +48,48 @@ sum(squares)  # step 2: sum them up
 result <- sum(x^2)  # two steps in one go
 
 
+# Exercise 2.8 (Vector wise computation 3) Square each value in the vector x, sum up the values, and divide by 3
+
+
+
+sum(x^2)/3
+mean(squares)
+
+
+
+
+# Exercise 2.9 (Compute the variance) 
+
+var(x)
+sd(x) 
+sqrt(var(x))
+
+
+
+
+# Exercise 2.10 (Work with NA)
+
+y <- c(1,2,NA)  # not available
+
+
+
+
+# how to "read" a variable
+x  # just enter its name and execute the line
+
+
+
+# Importing data
+
+
+
+
+
+
+
+
+
+
+
 
 
