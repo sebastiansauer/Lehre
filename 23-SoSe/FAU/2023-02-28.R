6eafb184affcf0e82b717b1e73470c0a64acc70b
Sebastian Sauer <ssauer@posteo.de>
2023-02-28 10:08:26 +0100

    computations

--- 23-SoSe/FAU/2023-02-28.R
+++ 23-SoSe/FAU/2023-02-28.R
@@ -3,4 +3,50 @@
 
 # Exercise 2.1 (Define a variable) Define in R the variable age and assign the value 42
 
+age <- 42  # Contrl-Enter
+
+
+
 # Exercise 2.2 (Define a variable as a string)
+
+name <- "me"
+name <- "name"
+
+
+
+# Exercise 2.3 (Define a variable by another variable) 
+
+age <- name
+
+
+# Exercise 2.4 (Call a function) 
+
+date()
+
+
+
+# Exercise 2.5 (Define a vector)
+
+x <- c(1, 2, 3)   # "list" - "vector"
+
+
+
+# Exercise 2.6 (Vector wise computation)
+
+x^2
+
+
+
+# Exercise 2.7 (Vector wise computation 2) 
+
+
+squares <- x^2  # step 1: square each element
+
+sum(squares)  # step 2: sum them up
+
+
+result <- sum(x^2)  # two steps in one go
+
+
+
+
