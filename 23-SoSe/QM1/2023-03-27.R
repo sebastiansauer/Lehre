7c8ab1dd982465a58cd6800999fd75ca422c341d
Sebastian Sauer <ssauer@posteo.de>
2023-03-27 11:06:56 +0200

    summarise goupby

--- 23-SoSe/QM1/2023-03-27.R
+++ 23-SoSe/QM1/2023-03-27.R
@@ -4,7 +4,10 @@
 
 # Vorbereitung:
 library(easystats)
-library(tidyverse)
+library(tidyverse)  # startet das Paket tidyverse
+
+
+# Verben des Datenjudos: Tidyverse -> Paket muss gestartet sein
 
 
 # Daten importieren:
@@ -14,3 +17,33 @@ mariokart <- data_read("https://vincentarelbundock.github.io/Rdatasets/csv/openi
 # Sortieren:
 
 mariokart2 <- arrange(mariokart, total_pr)
+
+arrange(mariokart, total_pr)
+
+
+
+# Filtern
+
+
+mariokart3 <- filter(mariokart, stock_photo == "yes" & cond == "new")
+
+
+# Summarise:
+
+summarise(mariokart, preis_mw = mean(total_pr))
+
+
+
+# Gruppieren und Zusammenfassen
+
+
+
+Zusammenfassen:
+  
+
+mariokart_gruppiert <- group_by(mariokart, stock_photo)
+mariokart_klein <- summarise(mariokart_gruppiert, max_preis = max(total_pr)) 
+mariokart_klein
+
+mariokart_klein %>% 
+  summarise(max_preis_mw = mean(max_preis))
