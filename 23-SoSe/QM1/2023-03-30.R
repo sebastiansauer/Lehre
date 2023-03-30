70f2c0a6f643f83197b532ffe3ba9d251326447b
Sebastian Sauer <ssauer@posteo.de>
2023-03-30 10:32:03 +0200

    Unterricht Syntax

---
+++ 23-SoSe/QM1/2023-03-30.R
@@ -0,0 +1,23 @@
+# 2023-03-30
+
+
+# Die Pfeife
+
+# Daten einlesen:
+
+library(easystats)  # Das Paket muss installiert sein
+mariokart <- data_read("https://vincentarelbundock.github.io/Rdatasets/csv/openintro/mariokart.csv")
+
+# Pakete starten:
+library(tidyverse)
+
+
+# Jetzt kommt die Pfeife, Kap. 4.6 (Shortcut: Strg-Shift-M):
+
+mariokart_wheels <- 
+  mariokart %>% 
+  group_by(wheels) %>% 
+  summarise(pr_mean = mean(total_pr),
+            count_n = n())  # n() gibt die Anzahl der Zeilen pro Gruppe an
+
+mariokart_wheels
