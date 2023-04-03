2ced329100b549dd8e20358b4c6e7ad7660e1d4c
Sebastian Sauer <ssauer@posteo.de>
2023-04-03 11:04:37 +0200

    2023-04-03

--- 23-SoSe/QM1/2023-03-30.R
+++ 23-SoSe/QM1/2023-03-30.R
@@ -21,3 +21,17 @@ mariokart_wheels <-
             count_n = n())  # n() gibt die Anzahl der Zeilen pro Gruppe an
 
 mariokart_wheels
+
+
+
+# Aufgabe "affairs_dplyr"
+
+
+
+
+affairs_path <- "https://vincentarelbundock.github.io/Rdatasets/csv/AER/Affairs.csv"
+affairs <- read.csv(affairs_path)
+
+affairs %>% 
+  filter(affairs > 0
+
