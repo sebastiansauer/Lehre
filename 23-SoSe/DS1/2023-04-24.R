5071b4aec3e0579aeba95e07cf92da81db803e4d
Sebastian Sauer <ssauer@posteo.de>
2023-04-24 09:56:54 +0200

    update

--- 23-SoSe/DS1/2023-04-24.R
+++ 23-SoSe/DS1/2023-04-24.R
@@ -9,16 +9,27 @@ mtcars2 <-
   mutate(am_f = factor(am)) %>% 
   select(-am)
 
+
 fit1 <- fit(logistic_reg(),
             am_f ~ ., data = mtcars2)
 
 fit1
 
+# oder so:
+
+rec1 <- recipe(am_f ~ ., data = mtcars2)
+wf1 <-
+  workflow() %>% 
+  add_model(logistic_reg()) %>% 
+  add_recipe(rec1)
+
+fit_wf1 <- fit(wf1, mtcars2)
 
 mtcars3 <-
   mtcars2 %>% 
   cbind(predict(fit1, new_data = NULL))
 
+# Güte:
 
 accuracy(mtcars3,
          truth = am_f,
