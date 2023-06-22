73373967531ec985ca3c95ea900c33c07f8931d5
Sebastian Sauer <ssauer@posteo.de>
2023-06-22 10:11:34 +0200

    Lehre 2023-06-22

---
+++ 23-SoSe/QM1/2023-06-22.R
@@ -0,0 +1,23 @@
+---
+title: "Kap. 10.6"
+format: html
+date: today
+---
+
+
+# Daten importieren
+
+Train-Datensatz:
+
+```{r}
+mariokart_train <- read.csv("https://raw.githubusercontent.com/sebastiansauer/statistik1/main/daten/mariokart_train.csv")
+```
+
+
+
+Test-Datensatz:
+
+```{r}
+mariokart_test <- read.csv("https://raw.githubusercontent.com/sebastiansauer/statistik1/main/daten/mariokart_test.csv")
+```
+
