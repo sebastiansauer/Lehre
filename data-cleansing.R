cf74dd41e03322f087343597a4ee5d977ae54c68
Sebastian Sauer <ssauer@posteo.de>
2023-09-13 13:02:21 +0200

    update

--- data-cleansing.R
+++ data-cleansing.R
@@ -6,7 +6,7 @@
 
 data(mtcars)
 
-library(tidyverse)
+library(tidyverse)  # work horse for data wrangling!
 
 # exr1: cyl < 6 
 
@@ -26,13 +26,11 @@ ggscatter(tab2, x = "hp", y = "mpg")
 # remember that "tab2" must be defined
 
 
-# alternative:
-library(ggstatsplot)
-ggscatterstats(tab2, x = hp, y = mpg)  # different syntax to ggpubr
 
-# alternative:
+
+# alternative  ggplot:
 library(tidyverse)
-ggplot(data = tab2) +
+ggplot(data = mtcars) +
   aes(x = hp, y = mpg) +
   geom_point()
 
@@ -40,6 +38,84 @@ ggplot(data = tab2) +
 # ex4: same as, but mpg ~ cyl
 # bonus: add regression line (hint: see online docs)
 
+# alternative:
+library(ggstatsplot)
+ggscatterstats(tab2, x = hp, y = mpg)  # different syntax to ggpubr
+
+ggscatter(tab2, x = "hp", y = "mpg",
+               add = "reg.line")  # different syntax to ggpubr
+
+
+library(tidyverse)
+ggplot(data = tab2) +
+  aes(x = hp, y = mpg) +
+  geom_point() +
+  geom_smooth(method = "lm")
 
 
 
+# ex5: kick out missing values
+
+# create a missing value
+mtcars$mpg[1] <- NA
+
+
+tab3 <- drop_na(mtcars)
+
+
+# ex6: deal with extreme values
+mtcars$mpg[2] <- 1000  # makes artificial extreme value
+
+tab4 <- mtcars
+
+# pipe!
+tab5 <- 
+  tab4 %>%  # shortcut for pipe: contrl-shift-m
+  mutate(mpg_is_outlier = mpg > 50)  # mutate changes columns/variables
+
+
+# approach 1: kick it out
+
+tab6 <-
+  tab5 %>% 
+  filter(mpg_is_outlier == FALSE)
+
+# approach 2: change it to the mean, if outlier
+
+library(easystats)  # if not yet started
+describe_distribution(tab5)
+
+
+# mutating values depending on some case/condition:
+tab7 <- 
+tab6 %>% 
+  mutate(mpg = case_when(
+    mpg_is_outlier == TRUE ~ 50,
+    mpg_is_outlier == FALSE ~ mpg
+  ))
+
+
+# mind the difference:
+x = 10  # assigns 10 to the object x
+x == 10 # checks whether/if x equals 10
+
+
+
+# ex9: convert `wt` from 1000 pounds to kg
+# 
+
+
+tab8 <-
+  mtcars %>% 
+  mutate(wt_kg = (wt * 1000)/2)
+
+
+# ex10: mutate mpg to `log(mpg)`
+
+tab9 <-
+  mtcars %>% 
+  mutate(mpg_logged = log(mpg))  # log10 is also possible
+
+
+
+  
