49843a2d1bc9357d0ea60cad8fe4ed022d76ab5d
Sebastian Sauer <ssauer@posteo.de>
2023-09-13 12:06:25 +0200

    update

--- data-cleansing.R
+++ data-cleansing.R
@@ -10,7 +10,7 @@ library(tidyverse)
 
 # exr1: cyl < 6 
 
-filter(mtcars, cyl≥ < 6)
+filter(mtcars, cyl < 6)
 
 # exr2: save the filter tab as new tab
 
@@ -20,12 +20,26 @@ tab2 <- filter(mtcars, cyl < 6)
 # mpg: fuel economy (consumption)
 # hp: horse power
 
-library(ggpubr)
 
+library(ggpubr)
 ggscatter(tab2, x = "hp", y = "mpg")
-# remenber that "tab2" must be defined
+# remember that "tab2" must be defined
+
+
+# alternative:
+library(ggstatsplot)
+ggscatterstats(tab2, x = hp, y = mpg)  # different syntax to ggpubr
+
+# alternative:
+library(tidyverse)
+ggplot(data = tab2) +
+  aes(x = hp, y = mpg) +
+  geom_point()
 
 
 # ex4: same as, but mpg ~ cyl
 # bonus: add regression line (hint: see online docs)
+
+
+
 
