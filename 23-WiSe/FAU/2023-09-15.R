8486bab1faeaba3ec91ab5d5ca8618672273abe4
Sebastian Sauer <ssauer@posteo.de>
2023-09-15 12:17:34 +0200

    interaction

--- 23-WiSe/FAU/2023-09-15.R
+++ 23-WiSe/FAU/2023-09-15.R
@@ -28,3 +28,48 @@ r2(lm1)  # this function comes from `easystats`
 summary(lm1)
 
 
+
+m2 <- lm(mpg ~ am + hp + cyl, data = mtcars)
+
+parameters(m2)
+r2(m2)
+
+
+
+
+# ex: compute the following model and report coefficients:
+# mpg is a function of am and hp and the interaction of the two factors
+
+data(mtcars)  # not strictly needed, but useful
+
+
+m3 <- lm(mpg ~ hp + am + am:hp, data = mtcars)
+parameters(m3)
+
+
+
+mtcars %>% 
+  mutate(am = factor(am)) %>%  # it's a grouping var. (nominal), NO NUMBER, R!
+  ggplot() +
+  aes(x = hp, color = am, group = am, y = mpg) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+
+#ex: try to find an interaction in mtcars! explorative research
+
+# different visualization appraoch:
+
+
+mtcars2 <-
+  mtcars %>% 
+  mutate(am = factor(am))
+
+m4 <- lm(mpg ~ hp + am + am:hp, data = mtcars2)
+parameters(m4)
+
+
+estimate_relation(m4) %>% plot()
+
+
+
