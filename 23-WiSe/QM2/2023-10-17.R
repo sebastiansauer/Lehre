acbf4c07595f1ecb651f7484924b581e4c5c778f
Sebastian Sauer <sebastiansauer1@gmail.com>
2023-10-17 09:33:05 +0200

    Syntax

---
+++ 23-WiSe/QM2/2023-10-17.R
@@ -0,0 +1,12 @@
+# 2023-10-17
+
+# Aufgabe:
+# Geg: n=10 Münzwürfe mit fairer Münze
+# Ges: Pr. für x=1 Treffer
+
+
+dbinom(x = 1,  # Anzahl Treffer
+       size = 10,  # Größes des Versuchs, d.h. Anzahl (n) Münzen
+       prob = 1/2  # Wskt für einen Treffer
+       )
+
