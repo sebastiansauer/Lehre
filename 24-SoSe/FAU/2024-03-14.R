6cfc61ae9bad1dae829b5afb1f63337427149894
Sebastian Sauer <ssauer@posteo.de>
2024-03-14 08:50:46 +0100

    add fau workshop

---
+++ 24-SoSe/FAU/2024-03-14.R
@@ -0,0 +1,12 @@
+
+# Course "Modelling your research data"
+# 2024-03-014
+
+
+# start packages:
+library(tidyverse)
+library(easystats)
+
+
+# import data:
+penguins <- read.csv("https://vincentarelbundock.github.io/Rdatasets/csv/palmerpenguins/penguins.csv")
