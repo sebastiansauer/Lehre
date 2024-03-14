4f2471239e6dc258bc6c4845e66e4397919949dc
Sebastian Sauer <ssauer@posteo.de>
2024-03-14 10:02:30 +0100

    ex 1 sol

--- 24-SoSe/FAU/2024-03-14.R
+++ 24-SoSe/FAU/2024-03-14.R
@@ -21,11 +21,19 @@ parameters(lm1_freq)  # coming from R package `easystats`
 plot(parameters(lm1_freq))
 plot(estimate_expectation(lm1_freq))
 
-# Exercise:
+# Exercise 1:
 # Build a model using the penguins data
 # y: weight 
 # x: bill 
 
+
+# Is the average weight positively related with bill length?
+# "The longer the bill, the heaver the aninmal (on average)"
+
+
+lm2 <- lm(body_mass_g ~ bill_length_mm, data = penguins)
+parameters(lm2)
+plot(estimate_expectation(lm2))
 
 
 
