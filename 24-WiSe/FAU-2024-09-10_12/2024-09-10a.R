228a0f496f99084d260a8780aa84718cf2bb75e1
Sebastian Sauer <ssauer@posteo.de>
2024-09-10 10:06:42 +0200

    starting

---
+++ 24-WiSe/FAU-2024-09-10_12/2024-09-10a.R
@@ -0,0 +1,30 @@
+
+1+1  # Command+Enter
+
+
+# define a variable: use "=" or "<-" (smaller-than plus minus)
+first_name <- "Sebastian"   # Command+Enter
+
+other_name <- "Richard"
+
+first_name <- other_name
+
+
+
+# calling a function
+
+print("Hello World!")
+
+
+
+# defining a vector ("list" of values):
+
+x <- c(1, 2, 3)  # hit Command-Enter
+
+
+# Exercise:
+# Use the function "mean" to compute the mean of the variable x.
+
+
+
+
