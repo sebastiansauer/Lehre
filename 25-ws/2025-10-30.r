5a4ee0d585758189ff2d6d59f31f71244ed0caf8
Sebastian Sauer <ssauer@posteo.de>
2025-10-30 09:23:52 +0100

    syntax

--- 25-ws/2025-10-30.r
+++ 25-ws/2025-10-30.r
@@ -1,1 +1,34 @@
 # 2025-10-30
+
+# Binomialverteilung
+# Bsp: 3 Lose ziehen, 2 Gewinnlose/Treffer
+# Was ist die Wahrscheinlichkeit, genau 2 Gewinnlose zu ziehen?
+
+# Man kann einen Baum zeichnen!
+# Manb kann aber auch die Binomialverteilung verwenden mit Hilfe von R.
+
+dbinom(x = 2, size = 3, prob = 2/5)
+# Bsp: 5.2
+# x = Anzahl der Erfolge (Treffer)
+# size = Anzahl der Versuche
+# prob = Wahrscheinlichkeit für Erfolg bei einem Versuch
+
+
+dbinom(x = 1, size = 3, prob = 2/5)
+dbinom(x = 0, size = 3, prob = 2/5)
+dbinom(x = 3, size = 3, prob = 2/5)
+
+
+pbinom(q = 2, size = 3, prob = 2/5)
+# 0,1,2 Treffer - höchstens 2 Treffer
+
+
+
+
+
+# Wahrscheinlichkeitsverteilung plotten
+x <- 0:3
+y <- dbinom(x = x, size = 3, prob = 2/5)
+plot(x, y, type = "h", lwd = 2, 
+     main = "Binomialverteilung: n = 3, p = 2/5",
+     xlab = "Anzahl der Treffer", ylab = "Wahrscheinlichkeit")
