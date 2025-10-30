30c7481a4a08b83787e4c49bda1396ccb7ac5ef2
Sebastian Sauer <ssauer@posteo.de>
2025-10-30 09:46:08 +0100

    klausur raten

--- 25-ws/2025-10-30.r
+++ 25-ws/2025-10-30.r
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # 2025-10-30
 
 # Binomialverteilung
@@ -25,31 +25,20 @@ pbinom(q = 2, size = 3, prob = 2/5)
 
 
 
+# Github CoPilot - hilft beim Coden
 
 
-# Wahrscheinlichkeitsverteilung plotten
-x <- 0:3
-y <- dbinom(x = x, size = 3, prob = 2/5)
-plot(x, y, type = "h", lwd = 2, 
-     main = "Binomialverteilung: n = 3, p = 2/5",
-     xlab = "Anzahl der Treffer", ylab = "Wahrscheinlichkeit")
-||||||| 19aac4b
-# 2025-10-30
-=======
-# 2025-10-30
 
-# Binomialverteilung
-# Bsp: 3 Lose ziehen, 2 Gewinnlose/Treffer
-# Was ist die Wahrscheinlichkeit, genau 2 Gewinnlose zu ziehen?
+# Aufgabe:
+# Was ist die Wskt., die Klausur durch Raten zu bestehen? (d.h. min 50% richtig, also 10 oder mehr Punkte?)
+# s. Beispiel 5.4! - sehr ähnlich
 
-# Man kann einen Baum zeichnen!
-# Manb kann aber auch die Binomialverteilung verwenden mit Hilfe von R.
-
-dbinom(x = 2, size = 3, prob = 2/5)
-# Bsp: 5.2
-# x = Anzahl der Erfolge (Treffer)
-# size = Anzahl der Versuche
-# prob = Wahrscheinlichkeit für Erfolg bei einem Versuch
+# Schritt 1: Wskt, durchzufallen:
+# 0-9 Punkte - dann fällt man durch
+p_fail <- pbinom(q = 9, size = 20, prob = 0.5) #  Wskt 0-9 Treffer
+p_fail
+# Schritt 2: Wskt, zu bestehen:
+p_pass <- 1 - p_fail
+p_pass
 
 
->>>>>>> d9e77f99a6faad6a3a2a96f77ee693d349072916
