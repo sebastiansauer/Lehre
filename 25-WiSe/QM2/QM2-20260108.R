d60fe342210eded1e0312b74c72c41c5fbd0797a
Sebastian Sauer <ssauer@posteo.de>
2026-01-08 08:41:53 +0100

    20260108

---
+++ 25-WiSe/QM2/QM2-20260108.R
@@ -0,0 +1,6 @@
+# AUFGABE:
+# Für jede der 8 in Kapitel 11 vorgestellte Forschungsfrage
+# suchen Sie bitte ein passendes Beispiel im Datensatz
+# "Bike Sharing" (s. Aufgabe "bike05").
+# Formulieren Sie jeweils a) die Regressionsformel und b)
+# die Forschungsfrage als ganzer Satz.
