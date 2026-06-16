2a3ada3c6a6812cc9e2f946b45cb7b9be379452d
Sebastian Sauer <ssauer@posteo.de>
2026-06-16 10:29:15 +0200

    syntax fopro

--- 26_SoSe/FoPro/202606016.R
+++ 26_SoSe/FoPro/202606016.R
@@ -8,12 +8,13 @@ library(tidyverse)
 
 d_raw <-
   tibble(
-    start_time = "2026-06-16 08:00:00",
-    end_time = "2026-06-16 09:00:00"
+    start_time = c("2026-06-16 08:00:00", "2026-06-16 08:01:00"),
+    end_time = c("2026-06-16 09:00:00", "2026-06-16 08:02:00")
   )
 
 
 d_raw |> 
   mutate(start_time = as_datetime(start_time),
          end_time  = as_datetime(end_time)) |> 
-  mutate(duration = end_time - start_time)
+  mutate(duration = end_time - start_time) |> 
+  mutate(too_fast = duration < "5 mins")
